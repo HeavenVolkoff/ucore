e085900718dd28ef33e74d89b0602ca46ebdf695
Vítor Vasconcellos <vitor.vasconcellos@proton.me>
2025-10-05 01:00:49 -0300

    Install xdg-ninja and glow

--- ucore/install-ucore.sh
+++ ucore/install-ucore.sh
@@ -31,6 +31,23 @@ curl --fail --retry 15 --retry-all-errors -sSL \
     | tar -xzf - -C /usr/local/bin starship
 chmod +x /usr/local/bin/starship
 
+# Install xdg-ninja
+mkdir -p /tmp/xdg-ninja
+curl --fail --retry 15 --retry-all-errors -sSL \
+    https://github.com/b3nj5m1n/xdg-ninja/archive/refs/heads/main.tar.gz \
+    | tar -xzf - --strip-components 1 -C /tmp/xdg-ninja
+
+pushd /tmp/xdg-ninja
+
+install -Dm 0755 -T xdg-ninja.sh '/usr/local/bin/xdg-ninja'
+install -d '/usr/local/share/xdg-ninja/'
+cp -r programs '/usr/local/share/xdg-ninja/'
+install -Dm 0644 -t '/usr/local/share/doc/xdg-ninja/' LICENSE README.md
+install -Dm 0644 -t '/usr/local/share/man/man1/' man/xdg-ninja.1
+
+popd
+rm -rf /tmp/xdg-ninja
+
 # disable tuned service by default
 systemctl disable tuned.service
 
